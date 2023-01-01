95336f223867f291b3023b831b4d6dd970dfeeb3
Tejas Shinde <111195452+tejas3227@users.noreply.github.com>
2023-01-01 15:38:47 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,76 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php 
+$email = $_SESSION['email'];
+$password = $_SESSION['password'];
+if($email != false && $password != false){
+    $sql = "SELECT * FROM usertable WHERE email = '$email'";
+    $run_Sql = mysqli_query($con, $sql);
+    if($run_Sql){
+        $fetch_info = mysqli_fetch_assoc($run_Sql);
+        $status = $fetch_info['status'];
+        $code = $fetch_info['code'];
+        if($status == "verified"){
+            if($code != 0){
+                header('Location: reset-code.php');
+            }
+        }else{
+            header('Location: user-otp.php');
+        }
+    }
+}else{
+    header('Location: login-user.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $fetch_info['name'] ?> | Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+    nav{
+        padding-left: 100px!important;
+        padding-right: 100px!important;
+        background: #6665ee;
+        font-family: 'Poppins', sans-serif;
+    } 
+    nav a.navbar-brand{
+        color: #fff;
+        font-size: 30px!important;
+        font-weight: 500;
+    }
+    button a{
+        color: #6665ee;
+        font-weight: 500;
+    }
+    button a:hover{
+        text-decoration: none;
+    }
+    h1{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        font-size: 50px;
+        font-weight: 600;
+    }
+    a{
+        color: black;
+        font-size:20px;
+    }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+    <u><b><a class="navbar-brand" href="#" style=" text-align: center; border-width:1px; border-style:solid; border-color:#fff; padding: 10px;">User Login Successful</a></u></b>
+    <button type="button" class="btn btn-light"><a href="logout-user.php">Logout</a></button>
+    </nav>    
+    <br><h4>Welcome <?php echo $fetch_info['name'] ?></h4>
+
+
+    
+</body>
+</html>
